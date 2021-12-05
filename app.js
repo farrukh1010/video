bf859cec3f27a774c9a05398a639d6dffb8cde84
farrukh1010 <42179299+farrukh1010@users.noreply.github.com>
2021-12-05 16:42:17 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
+});
