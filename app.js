e04e7ff27186604c699e5815a40c8a75f29c88b0
farrukh1010 <42179299+farrukh1010@users.noreply.github.com>
2021-12-05 16:42:53 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const preloader = document.querySelector(".preloader");
 window.addEventListener("load", function () {
   preloader.classList.add("hide-preloader");
 });
+btn.addEventListener("click", function () {
+    if (!btn.classList.contains("slide")) {
+      btn.classList.add("slide");
+      video.pause();
+    } else {
+      btn.classList.remove("slide");
+      video.play();
+    }
+  });
